5f4cb16484110e311f7d6aa4aefa962026d9edee
Sverre H. Huseby <shh@thathost.com>
2023-11-07 17:29:16 +0100

    chore: use Unboundid LDAP instead of testcontainers (#27)

--- src/test/java/no/shhsoft/dirsearch/ldap/LdapHelperIntegrationTest.java
+++ src/test/java/no/shhsoft/dirsearch/ldap/LdapHelperIntegrationTest.java
@@ -15,32 +15,32 @@ import java.util.Map;
  */
 public final class LdapHelperIntegrationTest {
 
-    private static LdapContainer ldapContainer;
+    private static LdapServer ldap;
     private static LdapHelper ldapHelper;
 
     @BeforeClass
     public static void beforeClass() {
-        ldapContainer = new LdapContainer();
-        ldapContainer.start();
-        ldapHelper = LdapHelper.forConfig(ldapContainer.getConfig());
+        ldap = new LdapServer();
+        ldap.start();
+        ldapHelper = LdapHelper.forConfig(ldap.getConfig());
     }
 
     @AfterClass
     public static void afterClass() {
-        ldapContainer.stop();
+        ldap.stop();
     }
 
     @Test
     public void shouldFindOne() {
-        final Map<String, Map<String, List<String>>> result = ldapHelper.search(LdapContainer.PERSON2_CN);
+        final Map<String, Map<String, List<String>>> result = ldapHelper.search(LdapServer.PERSON2_CN);
         Assert.assertEquals(1, result.size());
     }
 
     @Test
     public void shouldBar() {
-        System.out.println(DirSearch.searchResultToJsonString(ldapHelper.search(LdapContainer.PERSON2_CN)));
-        System.out.println(DirSearch.searchResultToJsonString(ldapHelper.search(LdapContainer.PERSONS_GROUP_CN)));
-        System.out.println(DirSearch.attributesToJsonString(ldapHelper.get(LdapContainer.PERSONS_GROUP)));
+        System.out.println(DirSearch.searchResultToJsonString(ldapHelper.search(LdapServer.PERSON2_CN)));
+        System.out.println(DirSearch.searchResultToJsonString(ldapHelper.search(LdapServer.PERSONS_GROUP_CN)));
+        System.out.println(DirSearch.attributesToJsonString(ldapHelper.get(LdapServer.PERSONS_GROUP)));
     }
 
 }
