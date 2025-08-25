57d8dbe50cd1c641280e9c318f989ef71ceec430
Sverre H. Huseby <sverre.huseby@statnett.no>
2025-08-25 14:00:46 +0200

    foo

--- src/test/resources/cert/generate.sh
+++ src/test/resources/cert/generate.sh
@@ -20,13 +20,23 @@ openssl req -new -x509 -days "$VALIDITY_DAYS" -keyout ca.key -out ca.pem -subj "
 echo "Generating ca-signed certificate and key."
 CERT=signed-cert
 cat > conf.conf <<EOT
-[v3_ca]
-basicConstraints = CA:FALSE
+[req]
+distinguished_name = req_distinguished_name
+req_extensions = v3_req
+prompt = no
+[req_distinguished_name]
+CN = localhost
+[v3_req]
 keyUsage = digitalSignature, keyEncipherment
+extendedKeyUsage = serverAuth
+subjectAltName = @alt_names
+[alt_names]
+DNS.1 = localhost
+IP.1 = 127.0.0.1
 EOT
 openssl genrsa -out "$CERT.key" 2048
-openssl req -new -subj "/CN=127.0.0.1" -key "$CERT.key" -out "$CERT.csr"
-openssl x509 -req -CA ca.pem -CAkey ca.key -in "$CERT.csr" -out "$CERT.pem" -extensions client -extensions v3_ca -extfile ./conf.conf -days "$VALIDITY_DAYS" -CAcreateserial -passin "pass:$PASS"
+openssl req -new -config conf.conf -key "$CERT.key" -out "$CERT.csr"
+openssl x509 -req -CA ca.pem -CAkey ca.key -in "$CERT.csr" -out "$CERT.pem" -extensions v3_req -extfile ./conf.conf -days "$VALIDITY_DAYS" -CAcreateserial -passin "pass:$PASS"
 
 echo "Generating keystore"
 openssl pkcs12 -export -in "$CERT.pem" -inkey "$CERT.key" -out keystore.p12 -name "$CERT" -passout "pass:$PASS"
