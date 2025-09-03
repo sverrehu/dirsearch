add5f22793c8d534b25eeec3988bde278d8fbfd0
Sverre H Huseby <shh@thathost.com>
2025-09-03 19:29:40 +0200

    foo

--- src/main/java/no/shhsoft/dirsearch/model/Entry.java
+++ src/main/java/no/shhsoft/dirsearch/model/Entry.java
@@ -1,18 +1,27 @@
 package no.shhsoft.dirsearch.model;
 
 import java.util.Comparator;
+import java.util.List;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
 public final class Entry {
 
-    private static final Comparator<Entry> ENTRY_COMPARATOR = Comparator.comparing(Entry::getDn);
+    private static final Comparator<String> DN_COMPARATOR = String::compareToIgnoreCase;
     private final String dn;
-    private final SortedSet<Entry> members = new TreeSet<>(ENTRY_COMPARATOR);
-    private final SortedSet<Entry> memberOf = new TreeSet<>(ENTRY_COMPARATOR);
+    private final SortedSet<String> members = new TreeSet<>(DN_COMPARATOR);
+    private final SortedSet<String> memberOf = new TreeSet<>(DN_COMPARATOR);
 
     Entry(final String dn) {
         this.dn = dn;
+    }
+
+    void addMember(final List<String> dns) {
+        members.addAll(dns);
+    }
+
+    void addMemberOf(final List<String> dns) {
+        memberOf.addAll(dns);
     }
 
     public String getDn() {
--- src/main/java/no/shhsoft/dirsearch/model/EntryTranslator.java
+++ src/main/java/no/shhsoft/dirsearch/model/EntryTranslator.java
@@ -1,5 +1,8 @@
 package no.shhsoft.dirsearch.model;
 
+import no.shhsoft.json.impl.generator.HumanReadableJsonGeneratorImpl;
+import no.shhsoft.json.model.JsonObject;
+
 import java.util.List;
 import java.util.Map;
 
@@ -10,10 +13,20 @@ public final class EntryTranslator {
 
     public static Entry fromDnAndAttributes(final String dn, final Map<String, List<String>> attributes) {
         final Entry entry = new Entry(dn);
+        for (final Map.Entry<String, List<String>> attribute : attributes.entrySet()) {
+            final String attributeName = attribute.getKey();
+            if ("member".equals(attributeName) || "uniqueMember".equals(attributeName)) {
+                entry.addMember(attribute.getValue());
+            } else if ("memberOf".equals(attributeName)) {
+                entry.addMemberOf(attribute.getValue());
+            }
+        }
+        return entry;
     }
 
     public static String toJson(final Entry entry) {
-
+        final JsonObject object = new JsonObject();
+        return new HumanReadableJsonGeneratorImpl().generate(object);
     }
 
 }
